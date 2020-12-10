426c6f993c56d0ca3585906fb709e46f636ac081
Vince <vinnykipx@gmail.com>
2020-12-10 19:37:40 +0300

    serverless

--- server/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-$connection=new PDO("mysql:host=localhost; dbname=NTSA","vince","//matata11");
-
-}
-catch(PDOException  $e){
-    echo "Error connecting to the server ". $e->getMessage();
-}
-?>
